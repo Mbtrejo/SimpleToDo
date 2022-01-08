2e0b468f4a19b83b8d57856234928b3946e8fa0f
Marlon Trejo <mbtrejo11@icloud.com>
2022-01-07 20:35:49 -0800

    commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.simpletodo
+
+import android.os.Bundle
+import android.os.FileUtils
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils.writeLines
+import org.apache.commons.io.IOUtils.writeLines
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+
+    var listOfTasks = mutableListOf<String>()
+    lateinit var adapter:TaskItemAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val onLongClickListener = object : TaskItemAdapter.OnLongClickListener{
+            override fun onItemLongClicked(position: Int) {
+                listOfTasks.removeAt(position)
+
+                adapter.notifyDataSetChanged()
+
+                saveItems()
+            }
+
+        }
+
+
+
+            loadItems()
+
+       val recyclerView =  findViewById<RecyclerView>(R.id.recyclerView)
+
+          adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+
+        recyclerView.adapter = adapter
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+
+            val userInputtedTask = inputTextField.text.toString()
+
+            listOfTasks.add(userInputtedTask)
+
+            adapter.notifyItemInserted(listOfTasks.size - 1 )
+
+            inputTextField.setText("")
+
+            saveItems()
+
+        }
+
+    }
+
+    fun getDataFile() : File {
+
+        return File(filesDir,"data.txt")
+
+    }
+
+
+    fun saveItems() {
+        try {
+            org.apache.commons.io.FileUtils.writeLines(getDataFile(), listOfTasks)
+        }
+
+        catch (ioexception:IOException){
+            ioexception.printStackTrace()
+        }
+    }
+
+    fun loadItems(){
+        try {
+            listOfTasks = org.apache.commons.io.FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        }
+
+        catch (ioexception:IOException){
+            ioexception.printStackTrace()
+        }
+    }
+
+
+}
